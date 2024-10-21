989295fd54eb440ba73ffaecdc01f55f34c0749c
Eledutv <amadoreduardo60@gmail.com>
2024-10-21 17:30:40 -0600

    modificaccion

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
-let url = window.location.href
-let swLocation = "/Tiwtor_vengadores/sw.js";
+let url = window.location.href;
+let swLocation = "/Tiwtor_vengadores/sw.js"; // Asegúrate de que la ruta sea la correcta.
+
 if (navigator.serviceWorker) {
+    // Ajusta la ruta del service worker si estás en localhost
     if (url.includes('localhost') || url.includes('127.0.0.1')) {
-        swLocation = 'sw.js';
+        swLocation = '/sw.js';
     }
+
+    // Registro del service worker
     navigator.serviceWorker.register(swLocation)
+        .then(reg => console.log('Service Worker registrado correctamente en:', swLocation))
+        .catch(err => console.error('Error al registrar Service Worker:', err));
 }
 
-
-
-
-
 // Referencias de jQuery
-
 var titulo = $('#titulo');
 var nuevoBtn = $('#nuevo-btn');
 var salirBtn = $('#salir-btn');
@@ -29,13 +30,8 @@ var txtMensaje = $('#txtMensaje');
 // El usuario, contiene el ID del héroe seleccionado
 var usuario;
 
-
-
-
-// ===== Codigo de la aplicación
-
+// ===== Código de la aplicación =====
 function crearMensajeHTML(mensaje, personaje) {
-
     var content = `
     <li class="animated fadeIn fast">
         <div class="avatar">
@@ -47,22 +43,16 @@ function crearMensajeHTML(mensaje, personaje) {
                 <br/>
                 ${mensaje}
             </div>
-            
             <div class="arrow"></div>
         </div>
     </li>
     `;
-
     timeline.prepend(content);
     cancelarBtn.click();
-
 }
-
-
 
 // Globals
 function logIn(ingreso) {
-
     if (ingreso) {
         nuevoBtn.removeClass('oculto');
         salirBtn.removeClass('oculto');
@@ -74,44 +64,32 @@ function logIn(ingreso) {
         salirBtn.addClass('oculto');
         timeline.addClass('oculto');
         avatarSel.removeClass('oculto');
-
         titulo.text('Seleccione Personaje');
-
     }
-
 }
 
-
-// Seleccion de personaje
+// Selección de personaje
 avatarBtns.on('click', function () {
-
     usuario = $(this).data('user');
-
     titulo.text('@' + usuario);
-
     logIn(true);
-
 });
 
-// Boton de salir
+// Botón de salir
 salirBtn.on('click', function () {
-
     logIn(false);
-
 });
 
-// Boton de nuevo mensaje
+// Botón de nuevo mensaje
 nuevoBtn.on('click', function () {
-
     modal.removeClass('oculto');
     modal.animate({
         marginTop: '-=1000px',
         opacity: 1
     }, 200);
-
 });
 
-// Boton de cancelar mensaje
+// Botón de cancelar mensaje
 cancelarBtn.on('click', function () {
     modal.animate({
         marginTop: '+=1000px',
@@ -122,15 +100,12 @@ cancelarBtn.on('click', function () {
     });
 });
 
-// Boton de enviar mensaje
+// Botón de enviar mensaje
 postBtn.on('click', function () {
-
     var mensaje = txtMensaje.val();
     if (mensaje.length === 0) {
         cancelarBtn.click();
         return;
     }
-
     crearMensajeHTML(mensaje, usuario);
-
 });
