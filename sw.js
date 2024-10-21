cfcde252a2d50ea11a6cd2bdcafb48ccfb77fb4c
Eledutv <amadoreduardo60@gmail.com>
2024-10-21 11:41:18 -0600

    modificaccion

--- sw.js
+++ sw.js
@@ -6,16 +6,16 @@ const DYNAMIC_CACHE = 'dynamic-v2'
 const INMUTABLE_CACHE = 'inmutable-v1'
 
 const APP_SHELL = [
-    // '/',
-    'index.html',
-    'css/style.css',
-    'img/favicon.ico',
-    'img/avatars/hulk.jpg',
-    'img/avatars/ironman.jpg',
-    'img/avatars/spiderman.jpg',
-    'img/avatars/thor.jpg',
-    'img/avatars/wolverine.jpg',
-    'js/app.js'
+    './',
+    './index.html',
+    './css/style.css',
+    './img/favicon.ico',
+    './img/avatars/hulk.jpg',
+    './img/avatars/ironman.jpg',
+    './img/avatars/spiderman.jpg',
+    './img/avatars/thor.jpg',
+    './img/avatars/wolverine.jpg',
+    './js/app.js'
 ]
 
 const APP_SHELL_INMUTABLE = [
@@ -42,15 +42,17 @@ self.addEventListener('install', e => {
 
 self.addEventListener('activate', e => {
     const respuesta = caches.keys().then(keys => {
-        keys.foreach(key => {
-            if (key != STATIC_CACHE && key.includes('static')) {
-                return caches.delete(key)
-            }
+        return Promise.all(
+            keys.map(key => {
+                if (key != STATIC_CACHE && key.includes('static')) {
+                    return caches.delete(key)
+                }
 
-            if (key != DYNAMIC_CACHE && key.includes('dynamic')) {
-                return caches.delete(key)
-            }
-        });
+                if (key != DYNAMIC_CACHE && key.includes('dynamic')) {
+                    return caches.delete(key)
+                }
+            })
+        )
     })
     e.waitUntil(respuesta)
 })
